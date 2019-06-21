e7fc7828f16164c41a28abb4da88450aaf4054bb
trupti <trupti.tompe@successive.tech>
2019-06-21 12:48:46 +0530

    feat: chnages in table component

---
+++ src/layout/AuthLayout/AuthLayout.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Footer } from '../components';
+
+const AuthLayout = ({ children, ...rest }) => (
+  <>
+    {children}
+  </>
+);
+
+
+export default AuthLayout;
